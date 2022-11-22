cce137d7070ec45da79c0b19f7da51a116babde8
breaklunch <elias.ibanez@tecsup.edu.pe>
2022-11-22 16:52:00 -0500

    coad

---
+++ src/main/java/com/example/demo/controllers/ControladorHola.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControladorHola {
+   @RequestMapping("/hola")
+     public String hola(Model modelo) {
+	   
+	   modelo.addAttribute("mensaje","Hola desde thymeleaf");
+	   return "hola";
+	}
+}
+
